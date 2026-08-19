dc8bade15fdf68c4978407eacfaa538fa4f002d5
Azharuddin Syed <syedazharuddin0081@gmail.com>
2026-08-18 22:51:57 -0400

    Update init_database.sql for database creation logic
    Modified the init_database.sql script to include checks for existing database and ensure proper creation and schema setup.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,16 +1,21 @@
 
 USE master;
+GO
 
-CREATE DATABASE DataWarehouse;
 
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse
 END
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
 
 USE DataWarehouse;
-
+GO
+	
 CREATE SCHEMA Bronze;
 GO
 CREATE SCHEMA Silver;
